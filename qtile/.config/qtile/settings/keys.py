f6696bfffac7a426da47e5c387aa2669c2dd8902
Julian Gallego <juesgale@gmail.com>
2022-11-27 21:30:43 -0500

    hide show top and bottom bar

--- qtile/.config/qtile/settings/keys.py
+++ qtile/.config/qtile/settings/keys.py
@@ -49,8 +49,7 @@ keys = [Key(key[0], key[1], *key[2:]) for key in [
     ([mod, "control"], "q", lazy.shutdown()),
     ([mod], "r", lazy.spawncmd()),
 
-    ([mod, "shift"], "x", lazy.hide_show_bar("top")),
-    ([mod, "control"], "x", lazy.hide_show_bar("bottom")),
+    ([mod, "shift"], "x", lazy.hide_show_bar("all")),
 
     # ------------ App Configs ------------
 
