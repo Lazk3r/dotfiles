182d2c39adfc827579995fbf6e208bd32c1d9864
Julian Gallego <juesgale@gmail.com>
2025-01-04 21:39:54 -0500

    New lunarvim config

--- lvim/.config/lvim/lua/user/plugins.lua
+++ lvim/.config/lvim/lua/user/plugins.lua
@@ -1,15 +1,4 @@
 lvim.plugins = {
   "catppuccin/nvim",
-  -- "j-hui/fidget.nvim",
   "karb94/neoscroll.nvim",
-  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-  "is0n/jaq-nvim",
-  {
-    "0x100101/lab.nvim",
-    build = "cd js && npm ci",
-  },
-  {
-    'dccsillag/magma-nvim',
-    build = ':UpdateRemotePlugins'
-  },
 }
