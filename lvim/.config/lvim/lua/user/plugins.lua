ceac664dda592d362d7ceb9081c29a64ad72900f
Julian Gallego <juesgale@gmail.com>
2025-01-05 15:14:26 -0500

    Adding metals to lunarvim

--- lvim/.config/lvim/lua/user/plugins.lua
+++ lvim/.config/lvim/lua/user/plugins.lua
@@ -1,4 +1,10 @@
 lvim.plugins = {
   "catppuccin/nvim",
   "karb94/neoscroll.nvim",
+  {
+    "scalameta/nvim-metals",
+    config = function()
+      require("user.metals").config()
+    end,
+  },
 }
